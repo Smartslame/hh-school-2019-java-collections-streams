36e19127ef329b90ddf631b290031cda5c5095a1
Alexander Yakushkin <smartslame@yandex.ru>
2019-11-22 13:54:32 +0300

    Add Task1 solution

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -4,9 +4,8 @@ import common.Person;
 import common.PersonService;
 import common.Task;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
@@ -17,20 +16,26 @@ import java.util.stream.Collectors;
  */
 public class Task1 implements Task {
 
-  // !!! Редактируйте этот метод !!!
-  private List<Person> findOrderedPersons(List<Integer> personIds) {
-    Set<Person> persons = PersonService.findPersons(personIds);
-    return Collections.emptyList();
-  }
+    // !!! Редактируйте этот метод !!!
+    private List<Person> findOrderedPersons(List<Integer> personIds) {
+        Set<Person> persons = PersonService.findPersons(personIds);
 
-  @Override
-  public boolean check() {
-    List<Integer> ids = List.of(1, 2, 3);
+        Map<Integer, Person> personMap = persons.stream()
+                .collect(Collectors.toMap(Person::getId, Function.identity()));
 
-    return findOrderedPersons(ids).stream()
-        .map(Person::getId)
-        .collect(Collectors.toList())
-        .equals(ids);
-  }
+        return personIds.stream()
+                .map(personMap::get)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean check() {
+        List<Integer> ids = List.of(1, 2, 3);
+
+        return findOrderedPersons(ids).stream()
+                .map(Person::getId)
+                .collect(Collectors.toList())
+                .equals(ids);
+    }
 
 }
