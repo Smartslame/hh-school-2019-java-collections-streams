fda74a49001923f1d3652e2bc470dd3fc66697ad
Alexander Yakushkin <smartslame@yandex.ru>
2019-11-22 14:45:22 +0300

    Add comments to Task1

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -20,12 +20,16 @@ public class Task1 implements Task {
   private List<Person> findOrderedPersons(List<Integer> personIds) {
     Set<Person> persons = PersonService.findPersons(personIds);
 
+    //Мапа строится за линию, так как вставка происходит за O(1)
     Map<Integer, Person> personMap = persons.stream()
         .collect(Collectors.toMap(Person::getId, Function.identity()));
 
+    //Аналогично, получение элемента из мапы происходит за O(1), тогда составление финального списка занимает тоже O(n)
     return personIds.stream()
         .map(personMap::get)
         .collect(Collectors.toList());
+
+    //Следовательно итоговая асимптотика - O(n)
   }
 
   @Override
