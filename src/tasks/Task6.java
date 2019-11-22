9180425c563ecd9691b7fe00f875b68cddf3cc30
Alexander Yakushkin <smartslame@yandex.ru>
2019-11-22 13:55:51 +0300

    Add Task6 solution

--- src/tasks/Task6.java
+++ src/tasks/Task6.java
@@ -5,11 +5,9 @@ import common.Person;
 import common.Task;
 
 import java.time.Instant;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -20,21 +18,40 @@ import java.util.Set;
  */
 public class Task6 implements Task {
 
-  private Set<String> getPersonDescriptions(Collection<Person> persons,
-                                            Map<Integer, Set<Integer>> personAreaIds,
-                                            Collection<Area> areas) {
-    return new HashSet<>();
-  }
+    private Set<String> getPersonDescriptions(Collection<Person> persons,
+                                              Map<Integer, Set<Integer>> personAreaIds,
+                                              Collection<Area> areas) {
 
-  @Override
-  public boolean check() {
-    List<Person> persons = List.of(
-        new Person(1, "Oleg", Instant.now()),
-        new Person(2, "Vasya", Instant.now())
-    );
-    Map<Integer, Set<Integer>> personAreaIds = Map.of(1, Set.of(1, 2), 2, Set.of(2, 3));
-    List<Area> areas = List.of(new Area(1, "Moscow"), new Area(2, "Spb"), new Area(3, "Ivanovo"));
-    return getPersonDescriptions(persons, personAreaIds, areas)
-        .equals(Set.of("Oleg - Moscow", "Oleg - Spb", "Vasya - Spb", "Vasya - Ivanovo"));
-  }
+
+        Map<Integer, String> areasIdName = areas.stream().collect(Collectors.toMap(Area::getId, Area::getName));
+
+        Set<String> personDescriptions = new HashSet<>();
+        StringBuilder nameRegion = new StringBuilder();
+
+        for (Person person : persons) {
+
+            for (Integer areaId : personAreaIds.get(person.getId())) {
+                nameRegion.append(person.getFirstName());
+                nameRegion.append(" - ");
+                nameRegion.append(areasIdName.get(areaId));
+
+                personDescriptions.add(nameRegion.toString());
+                nameRegion.delete(0, nameRegion.length());
+            }
+        }
+
+        return personDescriptions;
+    }
+
+    @Override
+    public boolean check() {
+        List<Person> persons = List.of(
+                new Person(1, "Oleg", Instant.now()),
+                new Person(2, "Vasya", Instant.now())
+        );
+        Map<Integer, Set<Integer>> personAreaIds = Map.of(1, Set.of(1, 2), 2, Set.of(2, 3));
+        List<Area> areas = List.of(new Area(1, "Moscow"), new Area(2, "Spb"), new Area(3, "Ivanovo"));
+        return getPersonDescriptions(persons, personAreaIds, areas)
+                .equals(Set.of("Oleg - Moscow", "Oleg - Spb", "Vasya - Spb", "Vasya - Ivanovo"));
+    }
 }
